9b714ebc52d48857249ec9f39317b2fda0468bd2
Fearless-Badger <micah.najacht@gmail.com>
2025-04-06 03:08:28 -0500

    Update SQLAlchemy model

--- backend/Functions/alchemy_model.py
+++ backend/Functions/alchemy_model.py
@@ -5,8 +5,10 @@ Base = declarative_base()
 
 class Student_db(Base):
     __tablename__ = "user_data"
-    student_id = Column(Integer, primary_key=True, index=True)
-    fname = Column(String(20))
-    mname = Column(String(20))
-    lname = Column(String(20))
+    student_id = Column(Integer, 
+                        primary_key=True, 
+                        index=True)
+    fname = Column(String(32))
+    mname = Column(String(32))
+    lname = Column(String(32))
     score = Column(Integer)
